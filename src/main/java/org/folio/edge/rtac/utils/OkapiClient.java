3ca58cebd5c76cadb6153ac6e2d8613cfa75afc2
craigmcnally <thruhiker07@gmail.com>
2018-05-17 17:39:05 +0000

    fix bug where connections were prematurely closed

--- src/main/java/org/folio/edge/rtac/utils/OkapiClient.java
+++ src/main/java/org/folio/edge/rtac/utils/OkapiClient.java
@@ -1,5 +1,8 @@
 package org.folio.edge.rtac.utils;
 
+import static org.folio.edge.rtac.Constants.APPLICATION_JSON;
+import static org.folio.edge.rtac.Constants.TEXT_PLAIN;
+import static org.folio.edge.rtac.Constants.X_OKAPI_TENANT;
 import static org.folio.edge.rtac.Constants.X_OKAPI_TOKEN;
 
 import java.util.HashMap;
@@ -8,15 +11,23 @@ import java.util.concurrent.CompletableFuture;
 
 import org.apache.log4j.Logger;
 
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonObject;
 
 public class OkapiClient {
 
   private static final Logger logger = Logger.getLogger(OkapiClient.class);
 
+  public static final long DEFAULT_REQUEST_TIMEOUT = 3 * 1000L; // ms
+ 
   private final String okapiURL;
-  private final HttpClient httpClient;
+  private final HttpClient client;
   private final String tenant;
 
   protected final Map<String, String> defaultHeaders = new HashMap<>();
@@ -24,7 +35,7 @@ public class OkapiClient {
   public OkapiClient(Vertx vertx, String okapiURL, String tenant) {
     this.okapiURL = okapiURL;
     this.tenant = tenant;
-    httpClient = new HttpClient(vertx);
+    this.client = vertx.createHttpClient(new HttpClientOptions().setKeepAlive(false));
   }
 
   public CompletableFuture<String> login(String username, String password) {
@@ -34,7 +45,7 @@ public class OkapiClient {
     payload.put("username", username);
     payload.put("password", password);
 
-    httpClient.post(
+    post(
         okapiURL + "/authn/login",
         tenant,
         payload.encode(),
@@ -63,7 +74,7 @@ public class OkapiClient {
 
   public CompletableFuture<String> rtac(String titleId) {
     CompletableFuture<String> future = new CompletableFuture<>();
-    httpClient.get(
+    get(
         okapiURL + "/rtac/" + titleId,
         tenant,
         defaultHeaders, response -> response.bodyHandler(body -> {
@@ -94,7 +105,7 @@ public class OkapiClient {
 
   public CompletableFuture<Boolean> healthy() {
     CompletableFuture<Boolean> future = new CompletableFuture<>();
-    httpClient.get(
+    get(
         okapiURL + "/_/proxy/health",
         tenant, response -> response.bodyHandler(body -> {
           try {
@@ -115,5 +126,61 @@ public class OkapiClient {
 
   public void setToken(String token) {
     defaultHeaders.put(X_OKAPI_TOKEN, token);
+  }
+  
+  public void post(String url, String tenant, String payload, Handler<HttpClientResponse> responseHandler) {
+    post(url, tenant, payload, null, responseHandler);
+  }
+
+  public void post(String url, String tenant, String payload, Map<String, String> headers,
+      Handler<HttpClientResponse> responseHandler) {
+
+    if (logger.isTraceEnabled())
+      logger.trace("POST " + url + " Request: " + payload);
+
+    final HttpClientRequest request = client.postAbs(url);
+
+    // safe to assume application/json. I *think* Caller can still
+    // override
+    request.putHeader(HttpHeaders.CONTENT_TYPE.toString(), APPLICATION_JSON)
+      .putHeader(HttpHeaders.ACCEPT.toString(), String.format("%s, %s", APPLICATION_JSON, TEXT_PLAIN))
+      .putHeader(X_OKAPI_TENANT, tenant);
+
+    if (headers != null) {
+      request.headers().addAll(headers);
+    }
+
+    request.handler(responseHandler);
+
+    request.setTimeout(DEFAULT_REQUEST_TIMEOUT)
+      .end(payload);
+  }
+
+  public void get(String url, String tenant, Handler<HttpClientResponse> responseHandler) {
+    get(url, tenant, null, responseHandler);
+  }
+
+  public void get(String url, String tenant, Map<String, String> headers,
+      Handler<HttpClientResponse> responseHandler) {
+
+    logger.info(String.format("GET %s tenant: %s token: %s", url, tenant,
+        (headers != null) ? headers.get(X_OKAPI_TOKEN) : ""));
+
+    final HttpClientRequest request = client.getAbs(url);
+
+    request.putHeader(HttpHeaders.ACCEPT.toString(), String.format("%s, %s", APPLICATION_JSON, TEXT_PLAIN))
+      .putHeader(X_OKAPI_TENANT, tenant);
+
+    if (headers != null) {
+      request.headers().addAll(headers);
+    }
+
+    request.handler(responseHandler)
+      .setTimeout(DEFAULT_REQUEST_TIMEOUT)
+      .end();
+  }
+  
+  public void close() {
+    client.close();
   }
 }
